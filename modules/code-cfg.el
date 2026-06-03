80cbd442dc40576874c1e933e6700d755f302de2
zaack1337 <isaacgameramos@gmail.com>
2026-06-03 14:13:48 -0300

    start use 'use-package'

--- modules/code-cfg.el
+++ modules/code-cfg.el
@@ -1,9 +1,10 @@
-(require 'eglot)
-
+(use-package eglot
+  :ensure t
+  :init
 (add-hook 'prog-mode-hook #'eglot-ensure)
 (add-hook 'c-mode-hook #'eglot-ensure)
 (add-hook 'rust-mode-hook #'eglot-ensure)
 (add-hook 'python-mode-hook #'eglot-ensure)
-
+)
 
 (provide 'code-cfg)
