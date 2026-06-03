581fdfbcdb901071343d4c957e18e5bf083561e6
zaack1337 <isaacgameramos@gmail.com>
2026-06-02 22:27:34 -0300

    add more custom on dashboard

--- modules/dashboard-cfg.el
+++ modules/dashboard-cfg.el
@@ -1,11 +1,25 @@
 (require 'dashboard)
 (dashboard-setup-startup-hook)
 
-(setq dashboard-banner-logo-title "zaack1337 - Emacs")
-(setq dashboard-startup-banner (locate-user-emacs-file "banner.png"))
+;; nerd icons
+(setq dashboard-display-icons-p t)
+(setq dashboard-icon-type 'nerd-icons)
 
+;; Icones ao widgets do dash
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
+
+;; deixar conteudo no centro
 (setq dashboard-center-content t)
 
+;; adiciona items ao dashboard
+(setq dashboard-items '((recents   . 5)
+                        (projects  . 5)
+                        (agenda    . 5)))
+
+;; banner
+(setq dashboard-banner-logo-title "zaack1337 - Emacs")
+(setq dashboard-startup-banner (locate-user-emacs-file "banner.png"))
 (setq dashboard-image-banner-max-height 300)
 (setq dashboard-image-banner-max-width 300)
 
