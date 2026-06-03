be4a83a21df4bffd399032ea30e6894cd588c39f
zaack1337 <isaacgameramos@gmail.com>
2026-06-03 15:57:34 -0300

    remove elpa folder on the github

--- elpa/ultra-scroll-20260516.1225/ultra-scroll-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "ultra-scroll" "20260516.1225"
-  "Fast and smooth scrolling."
-  '((emacs "29.1"))
-  :url "https://github.com/jdtsmith/ultra-scroll"
-  :commit "f38653053b5c9bbe8dbcb6b2236ab8997fc2f9bb"
-  :revdesc "f38653053b5c"
-  :keywords '("convenience"))
